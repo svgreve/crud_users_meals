7b398753348a6289fc01e79ac46cdf5d3a068ec3
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-03-23 17:58:49 -0300

    Schema Meals / changeset

---
+++ lib/crud_meals/meals/meal.ex
@@ -0,0 +1,21 @@
+defmodule CrudMeals.Meals.Meal do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:description, :date_time, :calories]
+
+  schema "meals" do
+    field :description, :string
+    field :date_time, :naive_datetime
+    field :calories, :integer
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+  end
+end
