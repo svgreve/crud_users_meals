601f58bf2fb7a863a1feb9a4c3bd4b7f313dc49b
Sergio Vieira Greve <sgreve@eikon.com.br>
2021-03-26 09:12:22 -0300

    meals.create_test.exs

---
+++ test/crud_meals/meals/create_test.exs
@@ -0,0 +1,25 @@
+defmodule CrudMeals.Meals.CreateTest do
+  use CrudMeals.DataCase, async: true
+  import CrudMeals.Factory
+
+  alias CrudMeals.{Error, Meal}
+  alias CrudMeals.Meals.Create
+
+  describe "call/1" do
+    test "when all parameters are valid, returns the meal" do
+      params = build(:meal_params)
+      response = Create.call(params)
+      assert {:ok, %Meal{id: _id, calories: 1870}} = response
+    end
+
+    test "when there are invalid parameters, returns an error" do
+      params = build(:meal_params, %{date_time: "2012-03-32 08:00:00", calories: "aaa"})
+      response = Create.call(params)
+      {:error, %Error{result: changeset}} = response
+
+      expected_response = %{calories: ["is invalid"], date_time: ["is invalid"]}
+
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
